851e62eee7e5eead2fed83b343ccf4adf17143b3
Sebastián Gaviria <sebastiangaviria@outlook.com>
2021-11-18 09:25:55 -0500

    first routes

---
+++ README.md
@@ -0,0 +1,17 @@
+# Biblioteca virtual
+
+## Libros
+Colección de libros que existen en esta biblioteca.
+- Listar los libros existentes
+- Agregar nuevos libros
+- Ver información de un libro específico (autor, año de publicación, título, etc)
+- Buscar un libro específico por diferentes criterios (autor, título, etc)
+- Alquilar uno o varios libros
+
+
+## Usuarios
+Personas que utilizan la biblioteca
+- Registrar nuevo usuario
+- Iniciar sesión con un usuario existente
+- Ver y modificar perfil
+- Ver libros alquilados actualmente
--- index.ts
+++ index.ts
@@ -1,18 +1,41 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
-
+app.use(bodyParser.json());
 
 // get -> preguntar por información
 // post -> enviar nueva información
 // put -> modificar información que ya existe
 // delete -> borrar información
 
-
 app.get('/', (request, response) => {
+  response.send('home');
+});
 
-  response.send('hola cambio');
+const books = [
+  {
+    title: 'Dune'
+  },
+  {
+    title: 'Harry Potter'
+  }
+];
 
+app.get('/books', (request, response) => {
+  response.json(books);
+});
+
+app.post('/books', (request, response) => {
+  const newBook = {
+    title: request.body.title,
+  };
+  books.push(newBook);
+
+  response.json({
+    status: 'ok',
+    newBook
+  });
 });
 
 
